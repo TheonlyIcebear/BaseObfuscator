00df6d2c3baed62cce885fffbc628b8df37bf066
TheonlyIcebear <78031685+TheonlyIcebear@users.noreply.github.com>
2022-12-31 06:30:40 -0500

    Add files via upload

--- obfuscator.py
+++ obfuscator.py
@@ -1,12 +1,17 @@
-import base64, string, names, math
+import random, base64, string, names, math
 from tqdm import tqdm
 
-recursion = 2 # get's exponentially laggier, the higher this number, but more "encrypted"
-base = 72 # Must be a whole number, 1 - 72
+recursion = 10 # get's exponentially laggier, the higher this number, but more "encrypted"
+base = 93 # Must be a whole number, 1 - 94
+indent = 0 # How many indents should be used to space out the actual code and the pass. Used to hide the code from a IDE
 input = "example.py" # file to obfuscate
 
 code = open(input, "r+").read()
-key = ["#", "$", "@", "&", "^", "!", "&", "*", "<", ">", '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] + list(string.ascii_lowercase) + list(string.ascii_uppercase)
+key = characters = list(map(chr, range(33, 127)))
+key.remove('\\')
+key.remove("'")
+
+random.shuffle(key)
 
 def encode(x, base):
     if not x:
@@ -42,12 +47,10 @@ for n in tqdm(range(recursion)):
     enc = ' '.join([ str(encode(ord(chr), base)) for chr in code])
     
     if n+1 == recursion:
-        indent = '  '*20000
-        message = 'pass;'
+        message = f"pass{'  '*0};"
     else:
-        indent = ''
         message = ''
-    src = f"""{message}{indent}k={str(key).replace(' ', '')};(eval(''.join([chr(sum([k.index(str(ch))*({base}**c) for c, ch in enumerate(str(x)[::-1])]))for x in('{enc2}'.split(' '))])))(''.join([chr(sum([k.index(str(ch))*({base}**c) for c, ch in enumerate(str(x)[::-1])]))for x in('{enc}'.split(' '))]))"""
+    src = f"""{message}k={str(key).replace(' ', '')};(eval(''.join([chr(sum([k.index(str(ch))*({base}**c) for c, ch in enumerate(str(x)[::-1])]))for x in('{enc2}'.split(' '))])))(''.join([chr(sum([k.index(str(ch))*({base}**c) for c, ch in enumerate(str(x)[::-1])]))for x in('{enc}'.split(' '))]))"""
     code = src.replace('-.', '-1')
 
 with open('output.py', 'wb') as file:
