d58d72f90bd25e313ed016f7cc79ba65b1619e7a
TheonlyIcebear <78031685+TheonlyIcebear@users.noreply.github.com>
2023-01-21 00:30:12 -0500

    Add files via upload

--- obfuscator.py
+++ obfuscator.py
@@ -1,11 +1,12 @@
-import random, base64, string, names, math
+import random, base64, string, names, zlib, math
 from tqdm import tqdm
 
-recursion = 2 # get's exponentially laggier, the higher this number, but more "encrypted"
+recursion = 1 # get's exponentially laggier, the higher this number, but more "encrypted"
 base = 15000 # Must be a whole number, 2 - 55000
 indent = 0 # How many indents should be used to space out the actual code and the pass. Used to hide the code from a IDE
 bytes_allowed = True # If disabled then base cannot be above 93
 invisble_characters = True # Use invisible characters
+compress = False # compress code
 input = "example.py" # file to obfuscate
 
 
@@ -61,7 +62,7 @@ for n in tqdm(range(recursion)):
         message = f"pass{'  '*indent};"
         if invisble_characters:
             base = 2
-            key = ['​', '‍']
+            key = ['​', '‍', '‎']
     else:
         message = ''
         random.shuffle(key)
@@ -70,9 +71,12 @@ for n in tqdm(range(recursion)):
     enc2 = ' '.join([ (str(encode(ord(chr), base))) for chr in 'exec'])
     enc3 = ' '.join([ (str(encode(ord(chr), base))) for chr in 'compile'])
     src = f"""{message}k='{''.join(key)}';(eval(eval(''.join([chr(sum([k.index(str(ch))*({hex(base)}**c) for c, ch in enumerate(str(x)[::-0x1])]))for x in('{enc3}'.split(' '))]))(''.join([chr(sum([k.index(str(ch))*({hex(base)}**c) for c, ch in enumerate(str(x)[::-0x1])]))for x in('{enc2}'.split(' '))]), "", dir(__builtins__)[0x69-1])))(eval(''.join([chr(sum([k.index(str(ch))*({hex(base)}**c) for c, ch in enumerate(str(x)[::-0x1])]))for x in('{enc3}'.split(' '))]))(''.join([chr(sum([k.index(str(ch))*({hex(base)}**c) for c, ch in enumerate(str(x)[::-0x1])]))for x in('{enc}'.split('‌'))]), "", dir(__builtins__)[0x6a-1]))"""
-    code = src.replace('-.', '-1')
+    if compress:
+        src = f'''eval(dir(__builtins__)[0x6a-1])(__import__('zlib').decompress(__import__('base64').b64decode(b'{base64.b64encode(zlib.compress(src.encode())).decode()}')).decode())'''
+    print(len(enc), len(code), len(enc), len(src))
+    # print(enc)
 
 with open(f'{input} (output).py', 'wb') as file:
     file.write(src.encode())
 
-print(src)
+# print(src)
